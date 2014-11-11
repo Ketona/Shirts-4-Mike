ead5e1d4ddf13c4ef423edbdd16d0499d7465ed8
Ketonia <ketevanzazarashvili.kz@gmail.com>
2014-11-12 00:08:40 +0400

    Add Contact page and create header.php file for the header of the site.

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-<html>
-<head>
-	<title>Shirts 4 Mike</title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
-	<link rel="shortcut icon" href="favicon.ico">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Shirts 4 Mike</a></h1>
-
-			<ul class="nav">
-				<li class="shirts"><a href="#">Shirts</a></li>
-				<li class="contact"><a href="#">Contact</a></li>
-				<li class="cart"><a href="#">Shopping Cart</a></li>
-			</ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
+<?php include('inc/header.php') ?>
 
 		<div class="section banner">
 
@@ -84,7 +59,7 @@
 				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
 			</ul>
 
-			<p>&copy;2012 Shirts 4 Mike</p>
+			<p>&copy; <?php echo date('Y'); ?> Shirts 4 Mike</p>
 
 		</div>
 	
